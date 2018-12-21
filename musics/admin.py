65c73d5fa3c76c8479ee4297e93fb466aa82065a
gr11n <gr11n@kimo.com>
2018-12-21 21:18:22 +0800

    Initial commit

---
+++ musics/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from musics.models import Music
+# Register your models here.
+from django.contrib import admin
+from musics.models import Music
+admin.site.register(Music)
